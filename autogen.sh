f466d007651931481c9fc22e7d375a863ae78ca1
jbj <devnull@localhost>
2002-06-09 17:13:12 +0000

    Sanity (after refreshing rpm.8 and rpmbuild.8).
    CVS patchset: 74
    CVS date: 2002/06/09 17:13:12

--- autogen.sh
+++ autogen.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
 
-ACV="Autoconf version 2.13"
-AMV="automake (GNU automake) 1.4"
-USAGE="
-You need to install:
-	libtool-1.3.5
-	autoconf-2.13
-	automake-1.4
-"
-
-[ "`autoconf --version`" != "$ACV" ] && echo "$USAGE" && exit 1
-[ "`automake --version | head -1 | sed -e 's/1\.4[a-z]/1.4/'`" != "$AMV" ] && echo "$USAGE" && exit 1
-
 aclocal
-autoheader
-automake
+automake -a -c
 autoconf
 
 if [ "$1" = "--noconfigure" ]; then 
